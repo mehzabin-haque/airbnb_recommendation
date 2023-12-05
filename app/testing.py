a2ba7b52fa3a7062084a36003fbad0dcb4a8121f
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-12-05 20:21:45 +0600

    testing using only pytest and api

--- app/testing.py
+++ app/testing.py
@@ -1,27 +1,12 @@
-import requests
 import pytest
+from your_streamlit_app_file import make_api_call, get_recommendations, get_simplified_recommendations
 
 def test_make_api_call():
-    # Mocking the response for testing purposes
-    expected_response = {"document_id": 123, "document_title": "Sample Listing", 
-                         "document_content": "Sample Content", "document_address": "Sample Address"}
-    # Mock the requests.post method
-    def mock_post(*args, **kwargs):
-        class MockResponse:
-            def __init__(self, json_data, status_code):
-                self.json_data = json_data
-                self.status_code = status_code
+    # Assuming the function `make_api_call` is meant to make an API call
+    # Replace with appropriate test logic or mocks
+    response = make_api_call("house in front of beach")
+    assert response is not None
+    assert 'document_id' in response
+    assert 'document_title' in response
+    # Add more assertions based on the expected API response
 
-            def json(self):
-                return self.json_data
-
-        return MockResponse(expected_response, 200)
-
-    # Monkeypatch the requests.post method
-    requests.post = mock_post
-
-    # Test the function with a mock user input
-    user_input = "house in front of beach"
-    response_data = make_api_call(user_input)
-
-    assert response_data == expected_response
